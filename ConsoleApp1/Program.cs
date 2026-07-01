a2cf5db0895d83d6047123ca3e78ada15fce677f
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-01 19:18:17 +0400

    first project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Please Enter Your name");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello {name}");
+
+Console.Write("Enter mark: ");
+int mark = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Mark: {mark}");
+
+string grade; 
+
+if (mark >= 90)
+{
+    grade = "A+";
+}
+else if (mark >= 80)
+{
+    grade = "A";
+}
+else if (mark >= 70)
+{
+    grade = "B";
+}
+else if (mark >= 60)
+{
+    grade = "C";
+}
+else
+{
+    grade = "F";
+}
+
+Console.WriteLine($"Grade: {grade}");
