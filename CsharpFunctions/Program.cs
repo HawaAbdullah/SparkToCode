c2d610d7c6ed599adcd2ec56edd0b81e26ce572d
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-06 00:16:03 +0400

    Add C# Functions practice project

---
+++ CsharpFunctions/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CsharpFunctions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double result =Math.Pow(2, 3);
+        Console.WriteLine("The Result is "+ result);
+        
+        
+
+
+
+    }
+}
