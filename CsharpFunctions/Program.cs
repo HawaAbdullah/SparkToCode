fd8f7d899de95e6c8cc6e8eb4aa07ca330dcd1b3
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-07-06 08:21:57 +0400

    User input math functions

--- CsharpFunctions/Program.cs
+++ CsharpFunctions/Program.cs
@@ -4,8 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        double result =Math.Pow(2, 3);
-        Console.WriteLine("The Result is "+ result);
+        Math.Cos(90);
+        Console.WriteLine("hello");
+        Console.Clear();
+
+        double r = Math.Cos(90);
+        Console.WriteLine(r);
+        double res =Math.Pow(2, 3);
+        Console.WriteLine("The Result is "+ res);
+        
+        // here the user will input math functions
+        Console.WriteLine("enter number:");
+        float userInput = float.Parse(Console.ReadLine());
+
+        double result = Math.Cos(userInput);
+        Console.WriteLine(result);
+        
+        
+
         
         
         
